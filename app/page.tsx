f48f3af74653fcb3ea2538d0b4226613eaea4fa3
kangminguu <mingudat9909@gmail.com>
2025-05-27 18:22:00 +0900

    feat : 축제 리스트 및 축제 이미지 api 호출 함수 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>;
+import getFestvalImage from "../lib/api/festival/getFestivalImage";
+import { getFestvalList } from "../lib/api/festival/getFestivalList";
+
+export default async function Page() {
+    // const festivalData = await getFestvalList({
+    //     pageNo: 1,
+    //     numOfRows: 9,
+    //     eventStartDate: "20250601",
+    //     eventEndDate: "20250627",
+    //     areaCode: 1,
+    //     keyword: "서울"
+    // });
+
+    // const imageList = await getFestvalImage("507480");
+
+    return (
+        <div>
+            next15
+        </div>
+    );
 }
---
+++ lib/api/festival/getFestivalList.ts
@@ -0,0 +1,51 @@
+interface GetFestivalListParams {
+    pageNo: number;
+    numOfRows: number;
+    eventStartDate: string;
+    eventEndDate: string;
+    keyword?: string;
+    areaCode?: number;
+}
+
+/**
+ * 행사정보조회 api 호출 함수
+ * @param pageNo 페이지 넘버
+ * @param numOfRows 한 페이지에 축제 수
+ * @param eventStartDate 축제 시작일
+ * @param eventEndDate 축제 종료일
+ * @param keyword 검색 키워드
+ * @param areaCode 지역 코드
+ * @returns festival : 축제 리스트와 개수 객체
+ */
+export async function getFestvalList(params: GetFestivalListParams) {
+    const url = new URL("searchFestival2", process.env.TourAPI_BASE_URL);
+    url.searchParams.set("serviceKey", decodeURIComponent(process.env.TourAPI_KEY));
+    url.searchParams.set("MobileOS", "ETC");
+    url.searchParams.set("MobileApp", "AppTest");
+    url.searchParams.set("_type", "json");
+    url.searchParams.set("pageNo", String(params.pageNo));
+    url.searchParams.set("numOfRows", String(params.numOfRows));
+    url.searchParams.set("eventStartDate", String(params.eventStartDate));
+    url.searchParams.set("eventEndDate", String(params.eventEndDate));
+    if (params.areaCode)
+        url.searchParams.set("areaCode", String(params.areaCode));
+
+    const res = await fetch(url.toString());
+    const data = await res.json();
+
+    const festival = {
+        festivalList: data.response.body.items.item, // 축제 리스트
+        totalCount: data.response.body.totalCount, // 축제 개수
+    };
+
+    // 키워드가 주어지면 필터링
+    if (params.keyword) {
+        festival.festivalList = festival.festivalList.filter(
+            (item: { title: string }) => item.title.includes(params.keyword)
+        );
+
+        festival.totalCount = festival.festivalList.length; // 키워드 필터링 후 축제 개수
+    }
+
+    return festival;
+}
