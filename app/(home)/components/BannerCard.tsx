ca210818adc1315aedf1da6c66a1b1bc94f13d6c
kangminguu <mingudat9909@gmail.com>
2025-05-30 14:55:00 +0900

    feature : Address 컴포넌트 개발

--- app/(home)/components/BannerCard.tsx
+++ app/(home)/components/BannerCard.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { convertToDotDateFormat, getToday } from "../../../lib/utils";
+import Address from "../../../components/Address/Address";
 
 export default function BannerCard({ festival }) {
-    const address = festival.addr1.split(" ").slice(0, 2).join(" ");
     const startDate = convertToDotDateFormat(festival.eventstartdate);
     const endDate = convertToDotDateFormat(festival.eventenddate);
     const thisMonth = parseInt(getToday().substring(4, 6)); // 이번 달
@@ -37,16 +37,7 @@ export default function BannerCard({ festival }) {
                     </div>
 
                     <div className="flex flex-col gap-[10px]">
-                        <div className="flex flex-row gap-[8px] text-font-primary">
-                            <img
-                                className="w-[24px]"
-                                src="/assets/location.svg"
-                                alt="location"
-                            />
-                            <p className="font-pretendard font-medium text-[20px]">
-                                {address}
-                            </p>
-                        </div>
+                        <Address address={festival.addr1} size={3} />
 
                         <div className="flex flex-row gap-[8px] text-font-primary">
                             <img
---
+++ components/Address/Adress.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Address from "./Address";
+
+const meta: Meta<typeof Address> = {
+    title: "Components/Address",
+    component: Address,
+    tags: ["autodocs"],
+    parameters: {
+        docs: {
+            description: {
+                component:
+                    "축제의 상태를 나타내는 Tag 컴포넌트입니다. 예정이면 태그를 표시하지 않습니다.",
+            },
+        },
+    },
+    argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof Address>;
+
+export const Address_size00: Story = {
+    args: {
+        address: "부산광역시 수영구 광안해변로 219",
+        size: 0,
+    },
+};
+export const Address_size01: Story = {
+    args: {
+        address: "부산광역시 수영구 광안해변로 219",
+        size: 1,
+    },
+};
+export const Address_size02: Story = {
+    args: {
+        address: "부산광역시 수영구 광안해변로 219",
+        size: 2,
+    },
+};
+export const Address_size03: Story = {
+    args: {
+        address: "부산광역시 수영구 광안해변로 219",
+        size: 3,
+    },
+};
