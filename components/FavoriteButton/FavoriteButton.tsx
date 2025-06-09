14bd5c140693edaa36e657c49cfb064c4f5eedef
kangminguu <131148077+kangminguu@users.noreply.github.com>
2025-06-09 11:15:13 +0900

    refactor : FavoriteButton 컴포넌트 분리 (#38)
    * refactor : FavoriteButton 컴포넌트 분리 및 스토리북 생성
    
    * chore : FestivalCard 스토리북 수정 및 FestivalCardSkeleton 타이틀 너비 수정

---
+++ components/FavoriteButton/FavoriteButton.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useFavoriteStore } from "../../stores";
+
+type FavoriteButtonSizeType = "card" | "detailPage";
+
+interface FavoriteButtonProps {
+    contentId: string;
+    sizeType?: string;
+}
+
+const styles = {
+    card: {
+        button: "absolute group md:w-[30px] w-[24px] top-[5px] right-[5px] md:top-[10px] md:right-[10px]",
+    },
+    detailPage: { button: "group w-[30px]" },
+};
+
+export default function FavoriteButton({
+    contentId,
+    sizeType = "card",
+}: FavoriteButtonProps) {
+    const { button } = styles[sizeType];
+    const { favorites, clickFavorite } = useFavoriteStore();
+
+    const handleFavorite = () => {
+        clickFavorite(contentId);
+    };
+
+    return (
+        <button
+            onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleFavorite();
+            }}
+            className={`${button}`}
+        >
+            {favorites.includes(contentId) ? (
+                <img
+                    src="/assets/favorite/favorite_active.svg"
+                    alt="favorite"
+                />
+            ) : (
+                <>
+                    <img
+                        src="/assets/favorite/favorite.svg"
+                        alt="favorite"
+                        className="group-hover:hidden block"
+                    />
+                    <img
+                        src="/assets/favorite/favorite_hover.svg"
+                        alt="favorite"
+                        className="hidden group-hover:block"
+                    />
+                </>
+            )}
+        </button>
+    );
+}
