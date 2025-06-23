46c03cda45a22f6cda773dc60179d8f4f925e133
kangminguu <mingudat9909@gmail.com>
2025-06-23 16:36:01 +0900

    feature : detail page id값이 유효하지 않는 경우 error 페이지

---
+++ app/detail/[id]/error.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Link from "next/link";
+import Button from "../../../components/Button/Button";
+import { useEffect } from "react";
+
+export default function Error({ error }: { error: Error }) {
+    useEffect(() => {
+        console.error("에러 발생:", error);
+    }, [error]);
+
+    return (
+        <div className="min-max-padding min-h-[400px] md:min-h-[600px] col-center gap-[10px] justify-center">
+            <img
+                src="/assets/error_page.svg"
+                className="w-[180px]"
+                alt="error_page"
+            />
+
+            <p>{"더 이상 존재하지 않거나 오류가 발생한 페이지입니다."}</p>
+
+            <Link href="/" className="mt-[20px]">
+                <Button
+                    title="홈으로 돌아가기"
+                    icon="/assets/undo.svg"
+                    isBorder
+                />
+            </Link>
+        </div>
+    );
+}
