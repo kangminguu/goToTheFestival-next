0b36a3f818b8bc4124ab0d7244a75ce030b0e3de
kangminguu <131148077+kangminguu@users.noreply.github.com>
2025-06-30 10:26:27 +0900

    bug : getFestivalList에 arrange값 추가, 수정일 순으로 변경 (#75)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -24,6 +24,7 @@ export default async function Page() {
         numOfRows: 6,
         eventStartDate: today,
         eventEndDate: lastDate,
+        arrange: "Q",
     });
 
     return (
--- lib/api/festival/getFestivalList.ts
+++ lib/api/festival/getFestivalList.ts
@@ -5,6 +5,7 @@ interface GetFestivalListParams {
     eventEndDate: string;
     keyword?: string;
     areaCode?: string;
+    arrange?: string;
 }
 
 /**
@@ -15,6 +16,7 @@ interface GetFestivalListParams {
  * @param eventEndDate 축제 종료일
  * @param keyword 검색 키워드
  * @param areaCode 지역 코드
+ * @param arrange 정렬 구분 (A=제목순, C=수정일순, D=생성일순) 대표이미지가반드시있는정렬(O=제목순, Q=수정일순, R=생성일순)
  * @returns festival : 축제 리스트, 축제 개수
  */
 export default async function getFestivalList(params: GetFestivalListParams) {
@@ -30,6 +32,7 @@ export default async function getFestivalList(params: GetFestivalListParams) {
     url.searchParams.set("numOfRows", String(params.numOfRows));
     url.searchParams.set("eventStartDate", String(params.eventStartDate));
     url.searchParams.set("eventEndDate", String(params.eventEndDate));
+    url.searchParams.set("arrange", String(params.arrange));
     if (params.areaCode)
         url.searchParams.set("areaCode", String(params.areaCode));
 
