5a12ff35ae8e0df2e413142ef1b08378151de8c0
kangminguu <131148077+kangminguu@users.noreply.github.com>
2025-07-01 16:03:48 +0900

    style : error page, not-found page 디자인 수정 및 title 추가 (#80)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
+// import { Metadata } from "next";
 import Alert from "../components/Alert/Alert";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import "./globals.css";
+
+// export const metadata: Metadata = {
+//     title: "축제가자",
+//     icons: {
+//         icon: "./icon.svg",
+//     },
+// };
 
 export default function RootLayout({
     children,
