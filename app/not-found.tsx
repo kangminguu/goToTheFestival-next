6bdd976390d8f2fb7b2ee85a4c68a53b9c9e3f4d
kangminguu <mingudat9909@gmail.com>
2025-07-01 15:26:34 +0900

    style : error page, not-found page 디자인 수정 및 title 추가

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,35 @@
+import { Metadata } from "next";
 import Link from "next/link";
 import Button from "../components/Button/Button";
+
+export const metadata: Metadata = {
+    title: "페이지를 찾을 수 없습니다 - 축제가자",
+    icons: {
+        icon: "./icon.svg",
+    },
+};
 
 export default function NotFound() {
     return (
         <div className="min-max-padding min-h-[400px] md:min-h-[600px] col-center gap-[10px] justify-center">
-            <img
-                src="/assets/error_page.svg"
-                className="w-[180px]"
-                alt="error_page"
-            />
+            <div className="col-center relative drag-prevent">
+                <span className="text-[64px] font-bold text-border-base">
+                    404
+                </span>
+                <img
+                    src="/assets/unconnected.png"
+                    className="w-[180px] absolute top-[64px]"
+                    alt="unconnected"
+                />
+            </div>
 
-            <p>{"더 이상 존재하지 않거나 오류가 발생한 페이지입니다."}</p>
+            <span>페이지를 찾을 수 없습니다.</span>
+
+            <span className="text-center text-[14px] text-font-secondary">
+                주소가 잘못 입력되었거나, 변경 또는 삭제되어
+                <br />
+                요청하신 페이지를 찾을 수 없습니다.
+            </span>
 
             <Link href="/" className="mt-[20px]">
                 <Button
