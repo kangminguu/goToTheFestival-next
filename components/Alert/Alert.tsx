711e54a01aa8ed3c59ea4ac491a70cc74ae1f7b2
kangminguu <mingudat9909@gmail.com>
2025-07-03 16:53:58 +0900

    bug : alert의 div 때문에 막혀서 클릭이 안되던것 수정

--- components/Alert/Alert.tsx
+++ components/Alert/Alert.tsx
@@ -8,9 +8,9 @@ export default function Alert() {
     if (!isOpen) return null;
 
     return (
-        <div className="w-full fixed top-[20px] flex justify-center z-50">
+        <div className="w-full fixed top-[20px] flex justify-center z-50 pointer-events-none">
             <div
-                className={`animate-fade-down animate-duration-1000 animate-alternate-reverse animate-delay-1000 bg-background-base w-[90%] md:w-[500px] rounded-[8px] px-[20px] py-[16px] row-center gap-[10px] justify-between shadow-alert`}
+                className={`animate-fade-down animate-duration-1000 animate-alternate-reverse animate-delay-1000 bg-background-base w-[90%] md:w-[500px] rounded-[8px] px-[20px] py-[16px] row-center gap-[10px] justify-between pointer-events-auto shadow-alert`}
             >
                 <span className="overflow-hidden">{content}</span>
 
