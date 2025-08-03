15b048f6e2e3d744b3c332c802fab427d851af3b
kangminguu <mingudat9909@gmail.com>
2025-08-03 12:25:49 +0900

    feature : 마이페이지 및 로그아웃, 회원 탈퇴 버튼 구성

--- app/mypage/components/DeleteAccountButton/DeleteAccountButton.tsx
+++ app/mypage/components/DeleteAccountButton/DeleteAccountButton.tsx
@@ -9,6 +9,9 @@ export default function DeleteAccountButton() {
     async function handleDelete() {
         await deleteAccount(); // 서버에서 계정 삭제
         await supabase.auth.signOut(); // 로그아웃
+
+        // 새로고침하면서 홈으로 이동을 위해
+        window.location.href = "http://localhost:3000/";
     }
 
     return <Button title="회원 탈퇴" onClick={handleDelete} />;
