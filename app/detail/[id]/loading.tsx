1a58d193ceb686e4e4ac4594f6c3cb3acec41461
kangminguu <mingudat9909@gmail.com>
2025-08-18 16:37:59 +0900

    feature : detailpage 로딩 화면 후기 섹션 스켈레톤 추가

--- app/detail/[id]/loading.tsx
+++ app/detail/[id]/loading.tsx
@@ -1,5 +1,4 @@
 import Button from "../../../components/Button/Button";
-import FavoriteButton from "../../../components/FavoriteButton/FavoriteButton";
 import DetailHeader from "./components/DetailHeader";
 
 export default function Loading() {
@@ -30,6 +29,25 @@ export default function Loading() {
                 <div className="md:h-[27px] h-[23px] w-[20%] bg-background-hover rounded" />
                 <div className="md:h-[27px] h-[23px] w-[20%] bg-background-hover rounded" />
                 <div className="md:h-[120px] h-[60px] w-full bg-background-hover rounded" />
+            </div>
+
+            {/* 축제 후기 */}
+            <div className="flex flex-col md:gap-[25px] gap-[20px] border border-border-base rounded-[8px] py-[16px] px-[14px] md:py-[36px] md:px-[30px] mb-[40px] mt-[20px]">
+                <div className="row-center justify-between">
+                    <h2 className="md:text-[24px] text-[16px] font-semibold">
+                        축제 후기
+                    </h2>
+                    <Button title="후기 작성" icon="/assets/open_in_new.svg" />
+                </div>
+                <div className="h-[21px] md:h-[24px] w-[20%] rounded bg-background-hover" />
+                <span className="w-full bg-border-base h-[1px]" />
+                <div className="row-center justify-between">
+                    <div className="h-[21px] md:h-[24px] w-[20%] rounded bg-background-hover" />
+                    <div className="h-[21px] md:h-[24px] w-[24px] rounded bg-background-hover" />
+                </div>
+
+                <div className="h-[21px] md:h-[24px] w-[70%] rounded bg-background-hover" />
+                <div className="h-[21px] md:h-[24px] w-[100%] rounded bg-background-hover" />
             </div>
 
             {/* 축제 상세 정보 */}
