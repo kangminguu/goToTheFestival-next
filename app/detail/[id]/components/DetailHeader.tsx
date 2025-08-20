2a83f957ff22dda366e543c925460356392f00fb
kangminguu <131148077+kangminguu@users.noreply.github.com>
2025-08-20 10:36:15 +0900

    feature : 탭 닫기 버튼 삭제 (#126)
    * feature : 탭 닫기 버튼 삭제
    
    * chore :

--- app/detail/[id]/components/DetailHeader.tsx
+++ app/detail/[id]/components/DetailHeader.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import Button from "../../../../components/Button/Button";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useAlertStore } from "../../../../stores/useAlertStore";
 
 export default function DetailHeader() {
-    const router = useRouter();
-
     const domain = process.env.NEXT_PUBLIC_BASE_URL;
     const urlPath = usePathname();
 
@@ -24,25 +22,8 @@ export default function DetailHeader() {
         }
     };
 
-    // 창 닫기
-    const handleClosedWindow = () => {
-        window.close();
-    };
-
     return (
-        <div className="row-center justify-between md:my-[20px] mb-[10px]">
-            <button
-                onClick={handleClosedWindow}
-                className="row-center font-semibold"
-            >
-                <img
-                    src="/assets/closed/closed_black.svg"
-                    alt="탭 닫기"
-                    className="w-[22px] h-[22px] mr-[5px]"
-                />
-                탭 닫기
-            </button>
-
+        <div className="row-center justify-end md:my-[20px] mb-[10px]">
             <Button
                 title="URL 복사"
                 icon="/assets/link.svg"
